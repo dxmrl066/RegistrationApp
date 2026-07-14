d1094e7d7e2db3222dc4ebc63508eb4eed7a9841
Nur Atiqah <25048302@myrp.edu.sg>
2026-07-14 14:03:06 +0800

    Database Credentials Updated

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'C237_regapp'
+    host: 'c237-yewyih-mysql.mysql.database.azure.com',
+    user: 'c237_006',
+    password: 'c237006@2026',
+    database: 'c237_006_l20_team3',
+    ssl: {}
 });
 
 db.connect((err) => {
